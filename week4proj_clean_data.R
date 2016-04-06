3fce9f5145bd033e76574e9c91e0950e096ed151
darpanamin <darpanamin@users.noreply.github.com>
2016-04-06 15:29:54 -0500

    Added files via upload

--- week4proj_clean_data.R
+++ week4proj_clean_data.R
@@ -1,7 +1,7 @@
 filePath <-"./Rstuff/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/"
 
 features<-read.delim(paste0(filePath,"features.txt"), header = FALSE,stringsAsFactors = FALSE, sep=" ")
-
+activity<-read.delim(paste0(filePath,"activity_labels.txt"), header = FALSE,stringsAsFactors = FALSE, sep=" ")
 ftest<-paste0(filePath,"test/")
 ftrain<-paste0(filePath,"train/")
 
@@ -9,7 +9,7 @@ file_list <- sapply(append(ftest,ftrain),list.files,pattern="*.txt",full.names =
 file_names <- sapply(append(ftest,ftrain),list.files,pattern="*.txt")
 myfiles = lapply(file_list,fread)
 names(myfiles) <-file_names
-
-total_data <-rbind(cbind(myfiles$subject_test.txt,myfiles$y_test.txt,myfiles$X_test.txt),cbind(myfiles$subject_train.txt,myfiles$y_train.txt,myfiles$X_train.txt))
-col_names<-append(c("subject","activity"),features$V2)
+activity_data_test <-merge(myfiles$y_test.txt,activity, by.x = "V1", by.y = "V1", all.x = TRUE)
+total_data <-rbind(cbind(myfiles$subject_test.txt,merge(myfiles$y_test.txt,activity, by.x = "V1", by.y = "V1", all.x = TRUE),myfiles$X_test.txt),cbind(myfiles$subject_train.txt,merge(myfiles$y_train.txt,activity, by.x = "V1", by.y = "V1", all.x = TRUE),myfiles$X_train.txt))
+col_names<-append(c("subject","activity_id","activity_name"),features$V2)
 names(total_data)<-col_names
